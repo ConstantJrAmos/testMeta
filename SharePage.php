ed19cca7226d68702634ff7b09e4ddba76979750
ConstantJrAmos <junior.a@service-formation.fr>
2022-03-24 10:24:05 +0100

    first commit 25

--- SharePage.php
+++ SharePage.php
@@ -14,7 +14,7 @@
 <body>
     <div class="les_button">
         <a class="button mt-6 ml-8"  href="/"> <i class="icon solid fa-laptop"></i></a>
-        <a class="button mt-6 ml-8" target="_blank"  href="https://www.facebook.com/sharer.php?u=https://app-metatest.herokuapp.com&title=Bonjour%20%C3%A0%20tous%20je%20m%27appelle%20%2C%20je%20suis%20sp%C3%A9cialis%C3%A9%20dans%20la%20vente%20en%20ligne%2C%20j%E2%80%99aide%20les%20salari%C3%A9s%20%C3%A0%20g%C3%A9n%C3%A9rer%20entre%203000%20et%2015000%20euros%20par%20mois%20gr%C3%A2ce%20%C3%A0%20Internet%2C%20avec%20des%20taux%20de%20commissions%20de%2050%25%20%2Ctr%C3%A8s%20int%C3%A9ressant%20surtout%20pour%20ceux%20ou%20celles%20qui%20font%20du%20digital%20Marketing%20cette%20opportunit%C3%A9%20pour%20rendre%20vos%20r%C3%A9seaux%20sociaux%20tr%C3%A8s%20rentables%20facilement.%20%20Contacter%20moi%20plus%20d%27informations.%20"> <i class="icon solid fa-share"></i></i></a>
+        <a class="button mt-6 ml-8" target="_blank"  href="https://www.facebook.com/sharer.php?u=https://app-metatest.herokuapp.com&quote=Bonjour à tous je m'appelle , je suis spécialisé dans la vente en ligne, j’aide les salariés à générer entre 3000 et 15000 euros par mois grâce à Internet, avec des taux de commissions de 50% ,très intéressant surtout pour ceux ou celles qui font du digital Marketing cette opportunité pour rendre vos réseaux sociaux très rentables facilement.  Contacter moi plus d'informations. Tel :  " > <i class="icon solid fa-share"></i></i></a>
     </div>
     <script>
         let title = "je suis spécialisé dans la vente en ligne, j’aide les salariés à générer entre 3000 et 15000 euros par mois grâce à Internet"
